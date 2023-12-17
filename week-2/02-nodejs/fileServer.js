2940978302bd989fa79fc9b129d875ded4bf00f7
paulpriyanshu <priyanshu.paul003@gmail.com>
2023-12-17 12:44:02 +0530

    solved fileServer problem

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -18,4 +18,30 @@ const path = require('path');
 const app = express();
 
 
+app.get('/files', function (req, res) {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+  if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
+  }
+  res.json(files);
+  });
+});
+
+
+app.get('/file/:name',(req,res)=>{
+  const filepath=path.join(__dirname,'./files/',req.params.name)
+  const file= fs.readFile(filepath,'utf-8',(err,data)=>{
+    if(err){
+     return  res.status(404).send('File not found')
+
+    }
+    res.send(data)
+  })
+
+
+})
+app.all('*',(req,res)=>{
+  res.status(404).send("Route not found")
+})
+
 module.exports = app;
